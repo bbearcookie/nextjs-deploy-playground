770d7d9dcea76ba2a89466e2aedc1db5d6e95132
bbearcookie <aodem@naver.com>
2024-12-29 17:00:39 +0900

    chore: 버전 출력

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import '@/styles/globals.css';
+
+import type { AppProps } from 'next/app';
+import packageJson from '../../package.json';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Next.js Deploy Playground - ${packageJson.version}</title>
+      </Head>
+      <div>버전: {packageJson.version}</div>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,7 +109,6 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-        <div>Hello!</div>
       </footer>
     </div>
   );
